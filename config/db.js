530b4ead54b302b9b30264dac793f7cc40cfb66a
Prachi Dhamne <prachi.0706@outlook.com>
2026-03-26 15:19:45 +0530

    fix db crash

--- config/db.js
+++ config/db.js
@@ -10,7 +10,7 @@ const db = mysql.createConnection({
 db.connect((err) => {
     if (err) {
         console.error('Database connection failed:', err.message);
-        process.exit(1);
+        console.log('Running without database connection');
     } else {
         console.log('Connected to MySQL database');
     }
